ab5af9c4cd9d89780f71dcc80085b6266007d4af
Hoon <hoonkong@gmail.com>
2013-11-29 18:33:44 -0800

    fully operational menu

--- js/augments.js
+++ js/augments.js
@@ -2,7 +2,8 @@ Object.prototype.inherits = function (Func) {
     "use strict"
     
     if (Func && typeof Func === "function") {
-        var F = new Func(arguments[1]);
+        console.log(Func.prototype.constructor.toString());
+        var F = new Func(arguments[1]);        
         for (var k in F) {
             if (F.hasOwnProperty(k)) {
                 this[k] = F[k];
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,30 @@
-var MenuGraph;
+var JsonMenu;
 (function (menuGraph) {
-    var MenuItem = function (text) {        
+    var MenuSuper = function(text) {
+    }
+    
+    var MenuItem = function (text) {
+        
+        this.inherits(MenuSuper, text);
+        
         var children;
+        var isVisible = true;
+        
         var domElement = (function () {
-            var miDiv = document.createElement("div");
+            var miDiv = document.createElement("div");            
             if (text) {
-                miDiv.appendChild(document.createTextNode(text));
-            }
+                miDiv.appendChild(document.createTextNode(text));                
+                miDiv.addEventListener("click", function (event) {
+                    var i;                    
+                    if (children && children.length && event.target === domElement)
+                    {                        
+                        for (i = 0; i < children.length; i++) {
+                            children[i].setVisible(!children[i].getVisible());
+                        }
+                    }
+                });              
+            }      
+            
             return miDiv;
         })();
         
@@ -27,12 +45,23 @@ var MenuGraph;
             return domElement;
         }
         
+        var setVisible = function (visible) {
+            isVisible = visible;
+            domElement.style.display = visible ? "block" : "none";
+        }
+        
+        var getVisible = function () {
+            return isVisible;
+        }
+        
         this.appendChildItem = appendChildItem;
         this.getDomElement = getDomElement;
         this.toString = toString;
+        this.setVisible = setVisible;
+        this.getVisible = getVisible;
     }
     
-    var createMenuHelper = function (menuJson, menuItem) {
+    var createMenuHelper = function (menuJson, menuItem, level) {
         var i;
         var parentItem = menuItem;
         
@@ -40,23 +69,26 @@ var MenuGraph;
         
         for (i = 0; i < menuJson.length; i++) {            
             menuItem = parentItem.appendChildItem(menuJson[i].text);
-            parentDomElement.appendChild(menuItem.getDomElement());
-            console.log(parentItem.toString() + " - " + menuItem.toString());
-            if (menuJson[i].children && menuJson[i].children.length && menuJson[i].children.constructor === Array) {                
-                createMenuHelper(menuJson[i].children, menuItem);
+            if (level > 0) {
+                menuItem.setVisible(false);
             }
-        }
+            parentDomElement.appendChild(menuItem.getDomElement());
+            //console.log(menuItem.toString() + " - lvl: " + level);
+            if (menuJson[i].children && menuJson[i].children.length && menuJson[i].children.constructor === Array) {                
+                createMenuHelper(menuJson[i].children, menuItem, level + 1);
+            }
+        }        
         
         return parentDomElement;
     }
     
     var createMenu = function (menuJson, containerId) {
-        var headDomElement = createMenuHelper(menuJson, new MenuItem());
+        var headDomElement = createMenuHelper(menuJson, new MenuItem(), 0);
         document.getElementById(containerId).appendChild(headDomElement);
     }
     
     menuGraph.createMenu = createMenu;
-})(MenuGraph || (MenuGraph = {}));
+})(JsonMenu|| (JsonMenu = {}));
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var menu = [
@@ -86,7 +118,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     text: "Menu item 3-3",
                     children: [
                         { text :"Menu item 3-3-1" },
-                        { text: "Menu item 3-3-2" },
+                        { 
+                            text: "Menu item 3-3-2",
+                            children: [
+                                { text :"Menu item 3-3-2-1" },
+                                { text: "Menu item 3-3-2-2" },
+                                { text: "Menu item 3-3-2-3" }
+                            ]
+                        },
                         { text: "Menu item 3-3-3" }
                     ]
                 },
@@ -113,5 +152,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     ];
     
-    MenuGraph.createMenu(menu, "menu");
+    JsonMenu.createMenu(menu, "menu");
 });
